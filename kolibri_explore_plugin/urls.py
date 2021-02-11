bc677669bbb6c0fcaadc2bccfa1c3f195abefb38
Daniel García Moreno <danigm@endlessos.org>
2021-02-11 13:12:09 +0100

    Avoid cache in AppView
    The cache is done by the browser and even with a fresh built endless
    key, the cache in the browser is used to render.
    
    This patch adds the plugin version to all file URLs to avoid browser
    caching.
    
    https://phabricator.endlessm.com/T31425

--- kolibri_explore_plugin/urls.py
+++ kolibri_explore_plugin/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
+from . import __version__ as VERSION
 from .views import AppFileView
 from .views import AppMetadataView
 from .views import AppView
@@ -7,17 +8,17 @@ from .views import ExploreView
 
 urlpatterns = [
     url(
-        r"^app/static/(?P<app>[\w\-]+)/(?P<filename>[\w\-.]+)",
+        r"^app/%s/static/(?P<app>[\w\-]+)/(?P<filename>[\w\-.]+)" % VERSION,
         AppFileView.as_view(),
         name="app_file",
     ),
     url(
-        r"^app/(?P<app>[\w\-]+)/metadata.json",
+        r"^app/%s/(?P<app>[\w\-]+)/metadata.json" % VERSION,
         AppMetadataView.as_view(),
         name="app_metadata",
     ),
     url(
-        r"^app/(?P<app>[\w\-]+)/(?P<path>.*)?",
+        r"^app/%s/(?P<app>[\w\-]+)/(?P<path>.*)?" % VERSION,
         AppView.as_view(),
         name="app_custom_presentation",
     ),
