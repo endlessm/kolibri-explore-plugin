d3714a1678fe348f627fde3c74d179610acb924e
Daniel García Moreno <danigm@endlessos.org>
2021-02-22 10:28:09 +0100

    Improve back button in IE11
    https://phabricator.endlessm.com/T31424

--- kolibri_explore_plugin/assets/src/app.js
+++ kolibri_explore_plugin/assets/src/app.js
@@ -23,6 +23,13 @@ class ExploreModule extends KolibriApp {
     return pluginModule;
   }
 
+  checkIE() {
+    // Detect it's IE11
+    const scrollLimit = '-ms-scroll-limit' in document.documentElement.style;
+    const imeAlign = '-ms-ime-align' in document.documentElement.style;
+    return scrollLimit && imeAlign;
+  }
+
   ready() {
     // after every navigation, block double-clicks
     router.afterEach((toRoute, fromRoute) => {
@@ -30,6 +37,19 @@ class ExploreModule extends KolibriApp {
       this.store.dispatch('resetModuleState', { toRoute, fromRoute });
     });
     super.ready();
+
+    if (this.checkIE()) {
+      window.addEventListener(
+        'hashchange',
+        () => {
+          var currentPath = window.location.hash.slice(1);
+          if (this.store.state.route.path !== currentPath) {
+            router.push(currentPath);
+          }
+        },
+        false
+      );
+    }
   }
 }
 
