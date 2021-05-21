a08fc99aa81dd94dc15a51628826d323ee8d853c
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-05-20 21:40:08 -0300

    Add eos-components package
    And make it a dependency in both the template and the discovery
    page. This was done calling:
    
    ```
    lerna add eos-components --scope=template-ui
    lerna add eos-components --scope=discovery-page
    ```
    
    https://phabricator.endlessm.com/T31931

---
+++ packages/eos-components/src/components/DummyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="count++">
+    I was clicked {{ count }} times.
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'DummyButton',
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+}
+</script>
