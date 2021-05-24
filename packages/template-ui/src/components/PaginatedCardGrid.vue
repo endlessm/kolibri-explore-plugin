0be6342791e4d3e27432c630518fd783dc8e6b2e
Daniel García Moreno <danigm@endlessos.org>
2021-05-24 17:54:42 +0200

    template: Clear pagination when the filter changes
    https://phabricator.endlessm.com/T32004

--- packages/template-ui/src/components/PaginatedCardGrid.vue
+++ packages/template-ui/src/components/PaginatedCardGrid.vue
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 
 export default {
   name: 'PaginatedCardGrid',
@@ -29,11 +30,23 @@ export default {
     };
   },
   computed: {
+    ...mapState(['filters']),
+    filterQuery() {
+      return this.filters.query;
+    },
     pageNodes() {
       const { currentPage, itemsPerPage, nodes } = this;
       const start = (currentPage - 1) * itemsPerPage;
       const end = start + itemsPerPage;
       return nodes.slice(start, end);
+    },
+  },
+  watch: {
+    filterQuery() {
+      // Change to first page if the filter changes. This is needed because the
+      // number of cards to show will change and the previous pagination can be
+      // wrong.
+      this.currentPage = 1;
     },
   },
 };
