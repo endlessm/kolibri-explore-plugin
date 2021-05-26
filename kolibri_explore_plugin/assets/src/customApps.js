70509e2e0a914716695cb023a34f420a84c2bef5
Simon Schampijer <simon.schampijer@endlessm.com>
2021-05-26 22:40:09 +0200

    Add override for TEDEd Think Like A Coder channel

--- kolibri_explore_plugin/assets/src/customApps.js
+++ kolibri_explore_plugin/assets/src/customApps.js
@@ -4,6 +4,7 @@ export const CustomChannelApps = {
   '1306b5d7d1ce4b98b0039324f368ce6a': 'pbskids',
   '3e464ee12f6a50a781cddf59147b48b1': 'sikana',
   '85461059d7d158f7ac03d050ddbf734e': 'healthy-living',
+  d3faf4e0feea4dd8b93d47e12081231f: 'teded-think-like-a-coder',
 };
 
 export function getAppNameByID(id) {
