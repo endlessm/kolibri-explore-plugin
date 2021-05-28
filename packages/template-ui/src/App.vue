3176b0decc6a617e65d2d8069475b15c00b977d6
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-05-28 17:49:46 -0300

    template: Route to a specific content or topic
    The route to a topic was already implemented, but it was broken
    because the routes changed. Also, add route to content. If both are
    given, content takes precedence.
    
    https://phabricator.endlessm.com/T31936

--- packages/template-ui/src/App.vue
+++ packages/template-ui/src/App.vue
@@ -58,9 +58,16 @@ export default {
       this.$store.commit('setHomeNavigation');
       const uri = window.location.search.substring(1);
       const params = new URLSearchParams(uri);
+      // Check if we need to navigate to a specific content or topic. Content takes precedence.
+      const contentId = params.get('contentId');
+      if (contentId) {
+        this.$router.push(`/c/${contentId}`);
+        return;
+      }
       const topicId = params.get('topicId');
       if (topicId) {
-        this.$router.push(`/${topicId}`);
+        this.$router.push(`/t/${topicId}`);
+        return;
       }
     },
   },
