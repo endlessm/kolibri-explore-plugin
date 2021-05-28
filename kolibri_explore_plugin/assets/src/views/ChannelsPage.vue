514ec67eff7cc337f161adbe3bcf187e1c5e4bda
Daniel García Moreno <danigm@endlessos.org>
2021-05-28 13:20:01 +0200

    Discovery: Add footer to the channels page
    https://phabricator.endlessm.com/T32021

--- kolibri_explore_plugin/assets/src/views/ChannelsPage.vue
+++ kolibri_explore_plugin/assets/src/views/ChannelsPage.vue
@@ -1,23 +1,56 @@
 <template>
 
-  <b-container class="channels pb-3 pt-3">
-    <!-- Cards without thumbnail -->
-    <ChannelCardGroup
-      :rows="rows.withThumbnail"
-      :hasThumbnail="true"
-      :columns="columns"
-      @card-click="goToChannel"
-    />
+  <div>
+    <div class="main">
+      <b-container class="channels pb-5 pt-3">
+        <!-- Cards without thumbnail -->
+        <ChannelCardGroup
+          :rows="rows.withThumbnail"
+          :hasThumbnail="true"
+          :columns="columns"
+          @card-click="goToChannel"
+        />
 
-    <!-- Cards without thumbnail -->
-    <ChannelCardGroup
-      :rows="rows.withoutThumbnail"
-      :hasThumbnail="false"
-      :columns="columns"
-      @card-click="goToChannel"
-    />
+        <!-- Cards without thumbnail -->
+        <ChannelCardGroup
+          :rows="rows.withoutThumbnail"
+          :hasThumbnail="false"
+          :columns="columns"
+          @card-click="goToChannel"
+        />
+      </b-container>
+    </div>
 
-  </b-container>
+    <div class="footer">
+      <b-container>
+        <b-row>
+          <b-col cols="7">
+            <h3 class="text-muted">
+              <strong>The Endless Key Discovery</strong> — content to help you do
+              your homework and discover curiosity you didn't even know you had,
+              even without internet.
+            </h3>
+          </b-col>
+          <b-col cols="1" />
+          <b-col>
+            <p class="text-muted">
+              The Endless Key initiative is brought to you by the Endless OS
+              Foundation in partnership with Common Sense and Learning
+              Equality
+            </p>
+            <b-button
+              pill
+              variant="outline-secondary"
+              target="_blank"
+              href="https://www.endlessos.org/key"
+            >
+              Learn more <b-icon-chevron-right />
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
 
 </template>
 
@@ -71,5 +104,14 @@
 <style lang="scss" scoped>
 
   @import '../styles';
+
+  .main {
+    background-color: white;
+  }
+
+  .footer {
+    padding: 5rem 0;
+    background: linear-gradient(to bottom, rgba($black, 0.075) 0, rgba($black, 0) 5px) $gray-300;
+  }
 
 </style>
