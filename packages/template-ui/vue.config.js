a05f64d765ce44718622efbcfe08f019769d7957
manuq <manuel.por.aca@gmail.com>
2021-05-28 14:28:47 +0200

    Update packages/template-ui/vue.config.js

--- packages/template-ui/vue.config.js
+++ packages/template-ui/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   publicPath: './',
   transpileDependencies: ['vue-clamp', 'resize-detector'],
   css: {
+    // FIXME this is because of IE11 compatibility:
     extract: false,
   },
 };
