2cd14fc5427a34c4e480ae6ad631b11a53e63741
Daniel García Moreno <danigm@endlessos.org>
2021-06-04 10:09:19 +0200

    Fix local fonts loading
    To make this work a new sass variable was declared,
    eos-components-fonts-path, that's used to calculate the real path of the
    fonts.
    
    In the template-ui it's easier to fix because we're using vue-cli and we
    can modify the way the css is loaded, so in this case we use the module
    resolve-url-loader to make it work the relative path.
    https://github.com/vuejs/vue-cli/issues/2099#issuecomment-639613478
    
    In the case of the plugin vue modules, I was unable to find the way to
    use the resolve-url-loader, but I was able to fix the problem just
    setting the variable to the real eos-components fonts path.

--- packages/template-ui/vue.config.js
+++ packages/template-ui/vue.config.js
@@ -5,4 +5,19 @@ module.exports = {
     // FIXME this is because of IE11 compatibility:
     extract: false,
   },
+  chainWebpack: (config) => {
+    ['vue-modules', 'vue', 'normal-modules', 'normal'].forEach(rule => {
+      config.module.rule('scss')
+        .oneOf(rule)
+        .use('resolve-url-loader')
+        .loader('resolve-url-loader')
+        .before('sass-loader')
+        .end()
+        .use('sass-loader')
+        .loader('sass-loader')
+        .tap(options =>
+          ({...options, sourceMap: true})
+        );
+    });
+  },
 };
