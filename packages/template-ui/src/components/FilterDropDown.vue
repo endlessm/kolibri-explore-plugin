6bd211c229af7f1b6bc1c48464044c1bf92e598a
Daniel García Moreno <danigm@endlessos.org>
2021-06-16 15:53:45 +0200

    Template: Reimplement filter dropdown as a buttons
    Some filters has a lot of options and the plain list is not usable. This
    patch changes that design to a list of buttons that takes less vertical
    space.
    
    There are also some changes to the app height, to adjust the popover
    window behavior, so it doesn't overflow the document and it's well
    positioned.
    
    https://phabricator.endlessm.com/T32082

---
+++ packages/template-ui/src/components/FilterDropDown.vue
@@ -0,0 +1,98 @@
+<template>
+  <b-dropdown
+    ref="dropdown"
+    class="mr-2"
+    :text="filter.prettyName"
+    :variant="filter.variant"
+    :boundary="boundary"
+  >
+    <div class="d-flex filter-dropdown flex-column">
+      <b-dropdown-header>
+        {{ filter.name }}
+        <b-button-close @click="closeFilter()" />
+      </b-dropdown-header>
+      <b-dropdown-divider class="my-1" />
+      <div class="filter-content flex-fill">
+        <b-btn
+          v-for="option in filter.options"
+          :key="option"
+          pill
+          class="mb-2 mr-2"
+          :variant="buttonVariant(filter, option)"
+          @click="onOptionClick({ filter, option, checked: !isSelected(filter, option) })"
+        >
+          {{ option }}
+        </b-btn>
+      </div>
+      <b-dropdown-divider class="my-1" />
+      <b-dropdown-form class="flex-shrink-0">
+        <b-button variant="link" @click="clearFilter({ filter })">
+          Clear
+        </b-button>
+      </b-dropdown-form>
+    </div>
+  </b-dropdown>
+</template>
+
+<script>
+  import { mapGetters, mapMutations } from 'vuex';
+
+  export default {
+    name: 'FilterDropDown',
+    props: {
+      filter: Object,
+    },
+    computed: {
+      ...mapGetters({
+        isSelected: 'filters/isSelected',
+      }),
+      boundary() {
+        return document.body;
+      },
+    },
+    methods: {
+      ...mapMutations({
+        onOptionClick: 'filters/setFilterQuery',
+        clearFilter: 'filters/clearFilterQuery',
+      }),
+      buttonVariant(filter, option) {
+        return this.isSelected(filter, option) ? 'primary' : 'secondary';
+      },
+      closeFilter() {
+        this.$refs.dropdown.hide(true);
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/styles.scss';
+
+  .filter-dropdown {
+    max-height: 30rem;
+  }
+
+  @include media-breakpoint-up(md) {
+    .filter-dropdown {
+      max-width: map-get($container-max-widths, 'md');
+    }
+  }
+
+  @include media-breakpoint-only(md) {
+    .filter-dropdown {
+      max-width: map-get($container-max-widths, 'sm');
+    }
+  }
+
+  .filter-content {
+    overflow-y: auto;
+    padding: $dropdown-item-padding-y $dropdown-item-padding-x;
+  }
+
+  .b-dropdown {
+    ::v-deep .dropdown-toggle {
+      border-radius: $rounded-pill;
+    }
+  }
+
+</style>
