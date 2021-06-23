c5902c5be2b8d9ec2eeac4226556ada535c7de4a
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-06-23 13:01:55 -0300

    template: Add basic "simple bundle" view
    Convert the Section view into a dynamic view, similar to the cards. If
    the section in the store is a simple bundle, use a custom view. Rename
    the previous Section to ListSection.
    
    The custom view is very basic for now. And it is expected that there
    will be a PlaylistSection view in the future.
    
    https://phabricator.endlessm.com/T31752

---
+++ packages/template-ui/src/views/ListSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    :style="{ backgroundImage: backgroundImageURL }"
+  >
+    <slot></slot>
+    <FilterContent />
+
+    <div v-if="isFilterEmpty">
+      <div v-if="isInlineLevel">
+        <CardGrid
+          v-for="subsection in section.children"
+          :id="subsection.id"
+          :key="subsection.id"
+          :nodes="subsection.children"
+          :mediaQuality="mediaQuality"
+          :cardColumns="cardColumns"
+        >
+          <b-row>
+            <SectionTitle :section="subsection" />
+          </b-row>
+        </CardGrid>
+      </div>
+      <div v-else>
+        <CardGrid
+          :id="section.id"
+          :key="section.id"
+          :nodes="section.children"
+          :mediaQuality="mediaQuality"
+          :cardColumns="cardColumns"
+          variant="paginated"
+        />
+      </div>
+    </div>
+    <div v-else>
+      <FilterResult :node="section" />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  name: 'ListSection',
+  computed: {
+    ...mapState(['section', 'cardColumns', 'mediaQuality']),
+    ...mapGetters({
+      isInlineLevel: 'isInlineLevel',
+      getAssetURL: 'getAssetURL',
+      isFilterEmpty: 'filters/isEmpty',
+    }),
+    backgroundImageURL() {
+      return this.getAssetURL('sectionBackgroundImage');
+    },
+  },
+};
+</script>
