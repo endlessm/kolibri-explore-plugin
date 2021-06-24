7412635cd4ac928bea8934a6f2b7b1c846f9db24
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-06-24 17:22:51 -0300

    template: Remove unneeded wrappers from views
    https://phabricator.endlessm.com/T32168

--- packages/template-ui/src/views/Content.vue
+++ packages/template-ui/src/views/Content.vue
@@ -1,37 +1,33 @@
 <template>
   <div>
-    <b-jumbotron
-      fluid
-    >
-      <template v-slot:default>
-        <b-row class="mt-3">
-          <b-col md="6" sm="12">
-            <h3>{{ content.title }}</h3>
-            <p class="mb-2">
-              {{ subtitle }}
-            </p>
-            <!-- eslint-disable vue/no-v-html -->
-            <div class="mb-2" v-html="content.description"></div>
-            <b-badge
-              v-for="tag in subjectTags"
-              :key="tag"
-              pill
-              variant="primary"
-              class="mb-1 mr-1"
-            >
-              {{ tag }}
-            </b-badge>
-          </b-col>
-          <b-col md="6" sm="12">
-            <b-link
-              @click="goToContent(content)"
-            >
-              <ContentImage :node="content" />
-            </b-link>
-          </b-col>
-        </b-row>
-      </template>
-    </b-jumbotron>
+    <b-container class="main-container">
+      <b-row class="mt-3">
+        <b-col md="6" sm="12">
+          <h3>{{ content.title }}</h3>
+          <p class="mb-2">
+            {{ subtitle }}
+          </p>
+          <!-- eslint-disable vue/no-v-html -->
+          <div class="mb-2" v-html="content.description"></div>
+          <b-badge
+            v-for="tag in subjectTags"
+            :key="tag"
+            pill
+            variant="primary"
+            class="mb-1 mr-1"
+          >
+            {{ tag }}
+          </b-badge>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-link
+            @click="goToContent(content)"
+          >
+            <ContentImage :node="content" />
+          </b-link>
+        </b-col>
+      </b-row>
+    </b-container>
     <CardGrid
       v-if="nextNodesInTopic.length"
       :nodes="nextNodesInTopic"
@@ -70,7 +66,7 @@ export default {
 <style lang="scss" scoped>
 @import '@/styles.scss';
 
-.jumbotron {
+.main-container {
   background-color: $white;
   background-size: cover;
   padding-top: $navbar-height;
