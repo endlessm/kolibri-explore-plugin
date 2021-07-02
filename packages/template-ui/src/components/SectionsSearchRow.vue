c5f139eea4c3c9318e7338312fa6b24feaff3a5f
Daniel García Moreno <danigm@endlessos.org>
2021-07-02 13:31:32 +0200

    template: Improve SectionsSearchRow responsiveness
    https://phabricator.endlessm.com/T32042

--- packages/template-ui/src/components/SectionsSearchRow.vue
+++ packages/template-ui/src/components/SectionsSearchRow.vue
@@ -1,19 +1,32 @@
 <template>
-  <b-container class="align-items-center d-flex my-4">
-    <MainSections />
-    <b-button-toolbar class="align-self-end ml-auto" keyNav aria-label="Search">
-      <b-button-group
-        class="mx-1 my-1"
-      >
-        <b-button
-          pill
-          variant="outline-dark"
-          to="/search"
-        >
-          <b-icon-search />
-          Search
-        </b-button>
-      </b-button-group>
-    </b-button-toolbar>
+  <b-container class="my-4">
+    <b-row>
+      <b-col sm="12" md="8" lg="10">
+        <MainSections />
+      </b-col>
+      <b-col>
+        <b-button-toolbar keyNav aria-label="Search">
+          <b-button-group
+            class="ml-md-auto mx-1 my-1"
+          >
+            <b-button
+              pill
+              class="search-button"
+              variant="outline-dark"
+              to="/search"
+            >
+              <b-icon-search />
+              Search
+            </b-button>
+          </b-button-group>
+        </b-button-toolbar>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
+
+<style lang="scss" scoped>
+  .search-button {
+    white-space: nowrap;
+  }
+</style>
