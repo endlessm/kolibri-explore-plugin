2ef7f2de89af5e97161b074f40cae8fbb0fa33b9
Daniel García Moreno <danigm@endlessos.org>
2021-07-02 12:48:17 +0200

    template: Use PlayButton for media type filter
    https://phabricator.endlessm.com/T32094

--- packages/template-ui/src/components/FilterDropDown.vue
+++ packages/template-ui/src/components/FilterDropDown.vue
@@ -13,16 +13,29 @@
       </b-dropdown-header>
       <b-dropdown-divider class="my-1" />
       <div class="filter-content flex-fill">
-        <b-btn
-          v-for="option in filter.options"
-          :key="option"
-          pill
-          class="mb-2 mr-2"
-          :variant="buttonVariant(filter, option)"
-          @click="onOptionClick({ filter, option, checked: !isSelected(filter, option) })"
-        >
-          {{ option }}
-        </b-btn>
+        <template v-if="isMediaFilter(filter)">
+          <PlayButton
+            v-for="option in filter.options"
+            :key="option.kind"
+            class="mb-2 mr-2"
+            :kind="option.kind"
+            :label="option.label"
+            :variant="buttonVariant(filter, option.kind)"
+            @click="onClick(filter, option)"
+          />
+        </template>
+        <template v-else>
+          <b-btn
+            v-for="option in filter.options"
+            :key="option"
+            pill
+            class="mb-2 mr-2"
+            :variant="buttonVariant(filter, option)"
+            @click="onClick(filter, option)"
+          >
+            {{ option }}
+          </b-btn>
+        </template>
       </div>
       <b-dropdown-divider class="my-1" />
       <b-dropdown-form class="flex-shrink-0">
@@ -51,6 +64,7 @@
       ...mapGetters({
         isSelected: 'filters/isSelected',
         isFiltering: 'filters/isFiltering',
+        isMediaFilter: 'filters/isMediaFilter',
       }),
       boundary() {
         return document.body;
@@ -69,6 +83,16 @@
       },
       clearDisabled(filter) {
         return !this.isFiltering(filter);
+      },
+      onClick(filter, option) {
+        if (this.isMediaFilter(this.filter)) {
+          const selected = this.isSelected(filter, option.kind);
+          this.onOptionClick({ filter, option: option.kind, checked: !selected });
+          return;
+        }
+
+        const selected = this.isSelected(filter, option);
+        this.onOptionClick({ filter, option, checked: !selected });
       },
     },
   };
