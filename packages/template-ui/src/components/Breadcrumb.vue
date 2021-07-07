bb31111b191677420de56e5ce9932545134ca9a7
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-07-07 11:30:47 -0300

    template: Limit breadcrumbs to 4 items
    As per new design spec. Slice the items to 4. Also add the "..." when
    sliced.
    
    https://phabricator.endlessm.com/T32233

--- packages/template-ui/src/components/Breadcrumb.vue
+++ packages/template-ui/src/components/Breadcrumb.vue
@@ -13,10 +13,10 @@
       v-if="node.ancestors && node.ancestors.length"
       class="bg-transparent breadcrumb flex-nowrap px-2"
     >
+      <li v-if="isShortened" class="active breadcrumb-item">
+        ...
+      </li>
       <template v-if="xs || sm">
-        <li v-if="node.ancestors.length > 1" class="active breadcrumb-item">
-          ...
-        </li>
         <li
           class="breadcrumb-item text-truncate"
         >
@@ -29,7 +29,7 @@
       </template>
       <template v-else>
         <li
-          v-for="a in node.ancestors"
+          v-for="a in node.ancestors.slice(-maxBreadcrumbs)"
           :key="a.id"
           class="breadcrumb-item text-truncate"
         >
@@ -67,6 +67,11 @@ export default {
   props: {
     node: Object,
   },
+  data() {
+    return {
+      maxBreadcrumbs: 4,
+    };
+  },
   computed: {
     ...mapState(['channel']),
     parentNode() {
@@ -74,7 +79,13 @@ export default {
         return null;
       }
       return this.node.ancestors[this.node.ancestors.length-1];
-    }
+    },
+    isShortened() {
+      if (!this.node.ancestors || !this.node.ancestors.length) {
+        return false;
+      }
+      return this.xs || this.sm || this.node.ancestors.length > this.maxBreadcrumbs;
+    },
   },
   methods: {
     getTopicUrl(n) {
