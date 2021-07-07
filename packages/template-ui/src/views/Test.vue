f39fda06927f8d7ec3361813fbe4d1894517a6c2
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-07-07 19:26:11 -0300

    template: Add cards to the test page
    Add topic and content cards with different variations and corner
    cases.
    
    Also, fix breadcrumb background color and improve tags testing.
    
    https://phabricator.endlessm.com/T32077

--- packages/template-ui/src/views/Test.vue
+++ packages/template-ui/src/views/Test.vue
@@ -114,7 +114,60 @@
     <hr>
 
     <h3>Badges:</h3>
-    <span class="badge badge-light badge-pill">Light</span>
+    <b-badge pill variant="light" class="mr-1">
+      one
+    </b-badge>
+    <b-badge pill variant="light" class="mr-1">
+      two
+    </b-badge>
+    <b-badge pill variant="light" class="mr-1">
+      three
+    </b-badge>
+
+    <hr>
+
+    <h3>Topic Cards:</h3>
+    <b-row>
+      <b-col
+        v-for="node in topicNodes"
+        :key="node.id"
+        lg="3"
+        md="6"
+        xs="12"
+      >
+        <Card :node="node" mediaQuality="regular" />
+      </b-col>
+    </b-row>
+
+    <hr>
+
+    <h3>Content Cards:</h3>
+    <b-row>
+      <b-col
+        v-for="node in contentNodes"
+        :key="node.id"
+        lg="3"
+        md="6"
+        xs="12"
+      >
+        <Card :node="node" mediaQuality="regular" />
+      </b-col>
+    </b-row>
+
+    <hr>
+
+    <h3>Cards size comparison:</h3>
+    <b-row>
+      <b-col
+        v-for="node in [...topicNodes.slice(0, 1), ...contentNodes.slice(0, 1)]"
+        :key="node.id"
+        lg="3"
+        md="6"
+        xs="12"
+      >
+        <Card :node="node" mediaQuality="regular" />
+      </b-col>
+    </b-row>
 
     <hr>
 
@@ -122,11 +175,127 @@
 </template>
 
 <script>
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
 export default {
   name: 'Test',
   data() {
     return {
-      sampleText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      sampleText: lorem,
+      topicNodes: [
+        {
+          id: 't0',
+          kind: 'topic',
+          title: 'A topic',
+          description: 'This is a topic.',
+          useDefaultThumbnail: true,
+        },
+        // Topic node with long title:
+        {
+          id: 't1',
+          kind: 'topic',
+          title: lorem,
+          description: '',
+          useDefaultThumbnail: true,
+        },
+        // Topic node with long description:
+        {
+          id: 't2',
+          kind: 'topic',
+          title: 'A topic',
+          description: lorem,
+          useDefaultThumbnail: true,
+        },
+        // Topic node with long title and description:
+        {
+          id: 't3',
+          kind: 'topic',
+          title: lorem,
+          description: lorem,
+          useDefaultThumbnail: true,
+        },
+      ],
+      contentNodes: [
+        {
+          id: 'c0',
+          kind: 'video',
+          title: 'A video',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        {
+          id: 'c1',
+          kind: 'html5',
+          title: 'An app',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        {
+          id: 'c2',
+          kind: 'exercise',
+          title: 'An exercise',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        {
+          id: 'c3',
+          kind: 'document',
+          title: 'A document',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        // FIXME: add bundle card here.
+        {
+          id: 'c4',
+          kind: 'audio',
+          title: 'An audio',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        // Content node with some tags:
+        {
+          id: 'c10',
+          kind: 'video',
+          title: 'A video with duration and other tags',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {'duration': ['160'], 'subject': ['games'], 'type': ['test'], 'level': ['beginner']},
+        },
+        // Content node with long title:
+        {
+          id: 'c11',
+          kind: 'html5',
+          title: lorem,
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+        },
+        // Content node with channel:
+        {
+          id: 'c12',
+          kind: 'exercise',
+          title: 'Card with channel',
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {},
+          channel: {'title': 'My channel'},
+        },
+        // Content node with long title and many tags (worst case):
+        {
+          id: 'c13',
+          kind: 'document',
+          title: lorem,
+          author: 'Endless',
+          useDefaultThumbnail: true,
+          structuredTags: {'subject': ['one', 'two', 'three', 'four', 'five', 'six']},
+        },
+      ],
     };
   },
 };
