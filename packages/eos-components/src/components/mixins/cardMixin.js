783246af34e96d02fd324981313ef68702d38d09
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-07-13 19:00:13 -0300

    components: Add new fallback thumbnail to topic cards
    From new specs.
    
    https://phabricator.endlessm.com/T32077

--- packages/eos-components/src/components/mixins/cardMixin.js
+++ packages/eos-components/src/components/mixins/cardMixin.js
@@ -1,12 +1,13 @@
 import { getThumbnail } from 'kolibri-api';
 import { getFirstStructuredTag } from '../../utils';
-import { StructuredTags } from '../../constants';
+import { MediaQuality, StructuredTags } from '../../constants';
 
 import AppThumb from '../../assets/thumbnails/app.jpg';
 import AudioThumb from '../../assets/thumbnails/audio.jpg';
 import BundleThumb from '../../assets/thumbnails/bundle.jpg';
 import DocumentThumb from '../../assets/thumbnails/document.jpg';
 import ExerciseThumb from '../../assets/thumbnails/exercise.jpg';
+import TopicThumb from '../../assets/thumbnails/topic.png';
 import VideoThumb from '../../assets/thumbnails/video.jpg';
 
 export default {
@@ -59,6 +60,9 @@ export default {
       return fn(name);
     },
     fallbackGetAsset() {
+      if (this.isBundle) {
+        return BundleThumb;
+      }
       switch (this.node.kind) {
         case 'audio':
           return AudioThumb;
@@ -70,8 +74,12 @@ export default {
           return VideoThumb;
         case 'html5':
           return AppThumb;
+        case 'topic':
         default:
-          return BundleThumb;
+          if (this.mediaQuality === MediaQuality.LOW) {
+            return BundleThumb;
+          }
+          return TopicThumb;
       }
     },
     getDuration() {
