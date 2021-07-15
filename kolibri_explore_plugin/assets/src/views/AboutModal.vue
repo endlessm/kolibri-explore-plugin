14de939b718cf3105be0713f2233f135d8327094
Manuel Quiñones <manuel.por.aca@gmail.com>
2021-07-15 17:48:56 -0300

    discovery page: Add build information
    Also, move the top margin to each section, so the scrolling space
    takes the whole modal body and titles look correct.
    
    https://phabricator.endlessm.com/T32283

--- kolibri_explore_plugin/assets/src/views/AboutModal.vue
+++ kolibri_explore_plugin/assets/src/views/AboutModal.vue
@@ -7,8 +7,8 @@
     centered
   >
     <b-container class="bg-white text-dark">
-      <b-row class="pt-3">
-        <b-col md="3" sm="12">
+      <b-row>
+        <b-col class="pt-3" md="3" sm="12">
           <b-list-group v-b-scrollspy:sections-column>
             <b-list-group-item
               v-for="item in menuItems"
@@ -24,7 +24,7 @@
         </b-col>
         <b-col id="sections-column" class="sections-column" md="9" sm="12">
           <div>
-            <h3 id="about-section" class="text-primary">
+            <h3 id="about-section" class="pt-3 text-primary">
               About
             </h3>
             <p>Placeholder text.</p>
@@ -38,14 +38,24 @@
             <p>Placeholder text.</p>
             <p>Placeholder text.</p>
             <hr>
-            <h4>Release information</h4>
-            <p>Placeholder text.</p>
-            <p>Placeholder text.</p>
-            <p>Placeholder text.</p>
-            <hr>
+            <template v-if="buildInfo">
+              <h4>Release information</h4>
+              <b-list-group class="bg-light p-3 rounded-lg">
+                <b-list-group-item class="bg-light">
+                  Release: {{ buildInfo.last_release }}
+                </b-list-group-item>
+                <b-list-group-item class="bg-light">
+                  Date: {{ buildInfo.date }}
+                </b-list-group-item>
+                <b-list-group-item class="bg-light">
+                  Commit: {{ buildInfo.commit }}
+                </b-list-group-item>
+              </b-list-group>
+              <hr>
+            </template>
           </div>
           <div>
-            <h3 id="support-section" class="text-primary">
+            <h3 id="support-section" class="pt-3 text-primary">
               Support
             </h3>
             <p>Placeholder text.</p>
@@ -61,7 +71,7 @@
             <hr>
           </div>
           <div>
-            <h3 id="credits-section" class="text-primary">
+            <h3 id="credits-section" class="pt-3 text-primary">
               Credits
             </h3>
             <p>Placeholder text.</p>
@@ -85,6 +95,8 @@
 
 <script>
 
+  import urls from 'kolibri.urls';
+
   export default {
     name: 'AboutModal',
     props: {
@@ -95,6 +107,7 @@
     },
     data() {
       return {
+        buildInfo: null,
         activeItemId: null,
         menuItems: [
           {
@@ -117,10 +130,22 @@
     },
     mounted() {
       this.activeItemId = this.menuItems[0].id;
+      this.getBuildInfo();
     },
     methods: {
       setActiveItem(event) {
         this.activeItemId = event.target.id;
+      },
+      getBuildInfo() {
+        const buildInfo = urls.static(`build-info.json`);
+        fetch(buildInfo)
+          .then(response => response.json())
+          .then(data => {
+            this.buildInfo = data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
       },
     },
   };
