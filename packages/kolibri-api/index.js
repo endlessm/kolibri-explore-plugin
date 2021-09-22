4024cd60744ab907b76e745bd29a77bd57ef3fb8
Manuel Quiñones <manuq@endlessos.org>
2021-09-22 20:55:12 -0300

    kolibri-api: Remove getThumbnail method
    https://phabricator.endlessm.com/T32533

--- packages/kolibri-api/index.js
+++ packages/kolibri-api/index.js
@@ -1,31 +1,5 @@
 // Communication with Kolibri frontend:
 
-export function getThumbnail(node) {
-  return new Promise((resolve, reject) => {
-    if (!window.frameElement) {
-      reject(new Error('Not an iframe window'));
-    }
-    window.addEventListener('message', function waitThumb(event) {
-      if (event.data.event && event.data.nameSpace === 'hashi'
-                && event.data.event === 'sendThumbnail'
-                && event.data.data.id === node.id) {
-        window.removeEventListener('message', waitThumb, false);
-        resolve(event.data.data.thumbnail);
-      }
-    }, false);
-
-    const nameSpace = 'customChannelPresentation';
-    const event = 'getThumbnail';
-    const data = node.id;
-    const message = {
-      event,
-      data,
-      nameSpace,
-    };
-    window.parent.postMessage(message, '*');
-  });
-}
-
 export function goToChannelList() {
   console.log('Go to channel list');
   const nameSpace = 'customChannelPresentation';
