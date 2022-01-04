b682890a66792d211ffa1523318a7cd2a4cf4811
Daniel García Moreno <danigm@endlessos.org>
2022-01-04 19:14:14 +0100

    template: Implement initial search in wikipedia DetailView
    https://phabricator.endlessm.com/T32928

--- packages/template-ui/channel-overrides/wikipedia/components/DetailView.vue
+++ packages/template-ui/channel-overrides/wikipedia/components/DetailView.vue
@@ -1,12 +1,66 @@
 <template>
   <b-container class="main-container" fluid>
+    <!-- TODO: move to a new commponent -->
     <b-navbar class="header px-0" type="dark" variant="dark">
       <b-container fluid>
         <b-navbar-nav>
+          <b-nav-item :active="showSearchBox" @click="onSearchClicked">
+            <b-icon-search />
+            Search
+          </b-nav-item>
           <WikiBreadcrumb :items="breadcrumb" @click="onBreadCrumbClick" />
         </b-navbar-nav>
       </b-container>
     </b-navbar>
+
+    <b-collapse id="searchbox" v-model="showSearchBox">
+      <b-container class="py-4">
+        <b-input-group>
+          <b-form-input
+            v-model="searchQuery"
+            placeholder="Search..."
+            type="search"
+            :disabled="searching"
+          />
+          <b-input-group-append>
+            <b-button variant="primary" :disabled="searching" @click="doSearch">
+              <b-icon-search v-if="!searching" />
+              <b-spinner v-else small />
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+        <div id="suggestions" class="text-center">
+          <b-button
+            v-for="suggest in suggestions"
+            :key="suggest.path"
+            variant="link"
+            @click="onSuggestClick(suggest)"
+          >
+            {{ suggest.title }}
+          </b-button>
+        </div>
+        <div v-if="searchResults.success" id="results">
+          <h2>
+            Top {{ searchResults.articles.length }} results for '{{ searchQuery }}'
+          </h2>
+          <ol class="zim-search-results-list">
+            <template v-for="(article, index) in searchResults.articles">
+              <li
+                :ref="`article${index}`"
+                :key="index"
+                class="zim-search-result-item"
+              >
+                <a href="#" @click.prevent="onSuggestClick(article)">
+                  {{ article.title }}
+                </a>
+                <p>{{ article.snippet }}&nbsp;&hellip;</p>
+              </li>
+            </template>
+          </ol>
+          <!-- TODO: show load more button and implement pagination for search -->
+        </div>
+      </b-container>
+    </b-collapse>
 
     <iframe
       ref="iframe"
@@ -22,6 +76,8 @@
 </template>
 
 <script>
+import _ from 'underscore';
+
 export default {
   name: 'DetailView',
   props: {
@@ -34,20 +90,37 @@ export default {
     return {
       height: 0,
       breadcrumb: [],
+      showSearchBox: false,
+      searchQuery: '',
+      searching: false,
+      suggestions: [],
+      searchResults: { success: false },
     };
   },
   computed: {
+    zimfile() {
+      const [zimfile] = this.node.files.filter((f) => f.extension === 'zim');
+      return zimfile;
+    },
     rooturl() {
       if (!this.node || !this.node.files || !this.node.files.length) {
         return '';
       }
-      const [zimfile] = this.node.files.filter((f) => f.extension === 'zim');
-      return `/zim/content/${zimfile.checksum}.zim`;
+      return `/zim/content/${this.zimfile.checksum}.zim`;
     },
     styles() {
       return {
         height: `${this.height}px`,
       };
+    },
+  },
+  watch: {
+    searchQuery() {
+      if (this.searchQuery.length <= 3) {
+        this.suggestions = [];
+        return;
+      }
+      _.debounce(this.doSuggest, 100)();
     },
   },
   methods: {
@@ -88,6 +161,80 @@ export default {
     },
     onBreadCrumbClick(link) {
       this.$refs.iframe.contentWindow.location = link.href;
+    },
+    onSuggestClick(suggest) {
+      const path = `/zim/content/${this.zimfile.checksum}.zim/${suggest.path}`;
+      this.$refs.iframe.contentWindow.location = path;
+      this.showSearchBox = false;
+    },
+    onSearchClicked() {
+      this.showSearchBox = !this.showSearchBox;
+    },
+    doSearch() {
+      this.searching = true;
+      this.suggestions = [];
+      this.apiSearch(this.searchQuery)
+        .then((response) => {
+          this.searching = false;
+
+          const { articles, count } = response;
+          const start = 0;
+          const end = start + articles.length;
+          const next = end < count ? end : null;
+
+          this.searchResults = {
+            query: this.searchQuery,
+            articles,
+            count,
+            next,
+            success: true,
+          };
+        })
+        .catch((error) => {
+          this.searching = false;
+          this.searchResults = { success: false };
+          console.log(error);
+        });
+    },
+    doSuggest() {
+      this.apiSearch(this.searchQuery, true, 10)
+        .then((r) => {
+          this.suggestions = this.filterDuplicateArticles(r.articles, 5);
+        });
+    },
+    filterDuplicateArticles(articles, limit) {
+      // The search suggestions API tends to return many articles which
+      // redirect to the same place, for example with minor spelling
+      // differences. We solve this by fetching a larger number of results
+      // than we will display, and removing the duplicate results.
+      const articlePaths = new Set();
+      return articles.reduce((result, article) => {
+        const path = article.redirect || article.path;
+        if (!articlePaths.has(path) && result.length < limit) {
+          articlePaths.add(path);
+          return result.concat(article);
+        } else {
+          return result;
+        }
+      }, []);
+    },
+    apiSearch(query, suggest = false, maxResults = 10, start) {
+      const data = {
+        query,
+        max_results: maxResults,
+      };
+      if (start) {
+        data.start = start;
+      }
+      if (suggest) {
+        data.suggest = suggest;
+      }
+      const { origin } = window.location;
+      const url = new URL(`${origin}/zim/search/${this.zimfile.checksum}.zim`);
+      for (const k in data) {
+        url.searchParams.append(k, data[k]);
+      }
+      return fetch(url).then(r => r.json());
     },
   },
 };
