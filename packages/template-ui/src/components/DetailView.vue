4fbc9bc68139e41f56f4097bc79efae94e22a3f8
Manuel Quiñones <manuq@endlessos.org>
2022-01-05 18:33:04 -0300

    template: Add "back" button in DetailView component
    Like the one in the Header, the button goes to the previous page in
    the history.
    
    https://phabricator.endlessm.com/T32923

--- packages/template-ui/src/components/DetailView.vue
+++ packages/template-ui/src/components/DetailView.vue
@@ -1,6 +1,16 @@
 <template>
   <b-container class="bg-white wrapper" fluid>
     <b-container class="main-container">
+      <b-button
+        href="#"
+        pill
+        variant="light"
+        size="sm"
+        class="my-3 pl-0"
+        @click="$router.go(-1)"
+      >
+        <span><ArrowLeft />Back</span>
+      </b-button>
       <h1>{{ title }}</h1>
       <p>{{ subtitle }}</p>
       <slot>
@@ -10,8 +20,13 @@
 </template>
 
 <script>
+import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
+
 export default {
   name: 'DetailView',
+  components: {
+    ArrowLeft,
+  },
   props: {
     title: {
       type: String,
@@ -28,7 +43,7 @@ export default {
 <style lang="scss" scoped>
 @import '@/styles.scss';
 
-$main-container-top: $navbar-height + $spacer * 4;
+$main-container-top: $navbar-height;
 $main-container-bottom: $spacer * 3.5;
 
 .main-container {
