8d50e9f655f0c37c03a9d1118fd0bf295400cd8f
Daniel García Moreno <danigm@endlessos.org>
2022-01-05 13:34:45 +0100

    template: Use ContentPage to show wikipedia content
    https://phabricator.endlessm.com/T32928

--- packages/template-ui/channel-overrides/wikipedia/components/DetailView.vue
+++ packages/template-ui/channel-overrides/wikipedia/components/DetailView.vue
@@ -1,12 +1,10 @@
 <template>
   <iframe
     ref="iframe"
-    class="custom-presentation-iframe"
+    class="content-iframe"
     scrolling="no"
     frameBorder="0"
-    :style="styles"
     :src="rooturl"
-    @load="adjustIframeHeightOnLoad"
   >
   </iframe>
 </template>
@@ -20,36 +18,12 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      height: 0,
-    };
-  },
   computed: {
     rooturl() {
-      if (!this.node || !this.node.files || !this.node.files.length) {
+      if (!this.node) {
         return '';
       }
-      const [zimfile] = this.node.files.filter((f) => f.extension === 'zim');
-      return `/zim/content/${zimfile.checksum}.zim`;
-    },
-    styles() {
-      return {
-        height: `${this.height}px`,
-      };
-    },
-  },
-  methods: {
-    adjustIframeHeightOnLoad() {
-      // Set height to 0 and in the next tick set to the iframe body scrollHeight.
-      // This is needed to adjust the content size when clicking on links,
-      // because the body could have a "height: 100%" style and in that case the
-      // height doesn't shrink.
-      this.height = 0;
-      window.scrollTo(0, 0);
-      this.$nextTick(() => {
-        this.height = this.$refs.iframe.contentWindow.document.body.scrollHeight;
-      });
+      return `/explore/#/content/${this.node.id}`;
     },
   },
 };
@@ -60,10 +34,11 @@ export default {
 
 $main-container-top: $navbar-height;
 
-.custom-presentation-iframe {
+.content-iframe {
+  position: fixed;
+  top: $main-container-top;
   width: 100%;
   height: 100vh;
-  margin-top: $main-container-top;
 }
 
 </style>
