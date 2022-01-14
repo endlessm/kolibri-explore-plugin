d8b318c77ea1ce87793b44756d59ffb4c391761b
Daniel García Moreno <danigm@endlessos.org>
2022-01-14 13:53:51 +0100

    Discovery: Add the BackToTop button
    https://phabricator.endlessm.com/T32922

---
+++ packages/eos-components/src/components/BackToTop.vue
@@ -0,0 +1,59 @@
+<template>
+  <transition name="fade">
+    <b-link
+      v-if="hasScrolled"
+      class="back-to-top"
+      @click="goToTop"
+    >
+      <ChevronUpCircle :size="50" />
+    </b-link>
+  </transition>
+</template>
+
+<script>
+  import _ from 'underscore';
+  import ChevronUpCircle from 'vue-material-design-icons/ChevronUpCircle.vue';
+
+  export default {
+    name: 'BackToTop',
+    components: { ChevronUpCircle },
+    data() {
+      return {
+        hasScrolled: false,
+      };
+    },
+    created() {
+      this.debouncedScroll = _.debounce(this.onScroll, 100);
+      window.addEventListener('scroll', this.debouncedScroll);
+    },
+    destroyed() {
+      window.removeEventListener('scroll', this.debouncedScroll);
+    },
+    methods: {
+      goToTop() {
+        window.scrollTo(0, 0);
+      },
+      onScroll() {
+        this.hasScrolled = window.scrollY !== 0;
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles.scss';
+
+  .back-to-top {
+    position: fixed;
+    bottom: $spacer;
+    right: $spacer;
+    z-index: $zindex-fixed;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    @include transition($transition-fade);
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
