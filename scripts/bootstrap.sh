a644351b76d3ae658f38a9dbecbc023c1402e3bc
Daniel García Moreno <danigm@endlessos.org>
2022-02-17 13:49:40 +0100

    CI: Set @vue/cli version to v4.5.15
    This fixes the issue with the last release of @vue/cli
    
    error open@8.4.0: The engine "node" is incompatible with this module.
    Expected version ">=12". Got "10.24.1"
    
    https://phabricator.endlessm.com/T33182

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,5 +5,5 @@
 pipenv install
 pipenv run nodeenv -p --node=10.24.1
 pipenv run npm install -g yarn@1.22.10
-pipenv run yarn global add @vue/cli
+pipenv run yarn global add @vue/cli@v4.5.15
 pipenv run yarn install
