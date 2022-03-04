98c902f3e3b9afab98a4fce854eb6a853d00f330
Daniel García Moreno <danigm@endlessos.org>
2022-03-04 12:32:52 +0100

    discovery: Emit resize event after fullscreen
    https://phabricator.endlessm.com/T33246

--- kolibri_explore_plugin/assets/src/app.js
+++ kolibri_explore_plugin/assets/src/app.js
@@ -64,6 +64,21 @@ class ExploreModule extends KolibriApp {
       );
     }
 
+    // Reemit the resize event on fullscreenchange to fix the problems with epub size
+    //
+    // There's a weird behaviour in the that produces a race condition with the
+    // full-screen resize event, making epub content not rendering correctly
+    // when toggling full-screen.
+    window.addEventListener(
+      'fullscreenchange',
+      () => {
+        setTimeout(() => {
+          window.dispatchEvent(new Event('resize'));
+        }, 300);
+      },
+      false
+    );
+
     window.kolibri = kolibriApi;
   }
 }
