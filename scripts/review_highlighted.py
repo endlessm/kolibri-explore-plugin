f02aebbd2e57360db0ca46d88bd1faaa01ac3a23
Manuel Quiñones <manuq@endlessos.org>
2022-03-07 13:58:41 -0300

    scripts: Improve review highlighted script
    - Use fetchone instead of fetchall as @dylan commented in the PR
    - Add option to print all found titles / IDs
    
    https://phabricator.endlessm.com/T33258

--- scripts/review_highlighted.py
+++ scripts/review_highlighted.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import getpass
 import json
 import os
@@ -12,6 +13,18 @@ kolibri_home = (
     f"/run/media/{username}/eoslive/KOLIBRI_DATA/preseeded_kolibri_home"
 )
 
+parser = argparse.ArgumentParser(
+    description="Review the ingested highlighted content "
+    "with a Kolibri Home database.",
+)
+parser.add_argument(
+    "--list-rows",
+    action="store_true",
+    help="Also list the title and ID of existing content.",
+)
+
+args = vars(parser.parse_args())
+
 # Connect to the database:
 db_path = os.path.join(kolibri_home, "db.sqlite3")
 connection = sqlite3.connect(db_path)
@@ -24,9 +37,12 @@ with open(HIGHLIGHTED_CONTENT_PATH, "r") as input_file:
     content_ids = list(chain.from_iterable(data.values()))
 
 # Output error for each content ID not found in the database:
-query = "SELECT title FROM content_contentnode WHERE id = ?"
+query = "SELECT title, id FROM content_contentnode WHERE id = ?"
 for content_id in content_ids:
     cursor.execute(query, (content_id,))
-    exists = len(cursor.fetchall()) > 0
-    if not exists:
-        print(f"ID not found in database: {content_id}")
+    row = cursor.fetchone()
+    if row is None:
+        print(f"Error: ID not found in database: {content_id}")
+    elif args["list_rows"]:
+        title, _id = row
+        print(f"{title} - {_id}")
