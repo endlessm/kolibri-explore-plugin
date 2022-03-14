71ea80d15adf2cb5008737e58b7ff898f265372a
Daniel García Moreno <danigm@endlessos.org>
2022-03-14 12:52:15 +0100

    Update node version to 16.x
    This patch updates the node version to 16.x and also updates the
    yarn.lock file.
    
    The node-sass dependency has been replaced with sass, because it's
    deprecated: https://sass-lang.com/blog/libsass-is-deprecated
    
    https://phabricator.endlessm.com/T33280

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,7 +3,7 @@
 # Setup Project
 
 pipenv install
-pipenv run nodeenv -p --node=10.24.1
+pipenv run nodeenv -p --node=16.14.0
 pipenv run npm install -g yarn@1.22.10
 pipenv run yarn global add @vue/cli@v4.5.15
-pipenv run yarn install
+pipenv run yarn install --ignore-engines
