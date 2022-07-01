092eb3995c3d9d5dfed5a65b2a3b2b2512a38531
Manuel Quiñones <manuq@endlessos.org>
2022-07-01 17:57:50 -0300

    Welcome screen: Implement permission explanation view
    Add a new PermissionsExplanation view and API to connect with the
    Android app.
    
    https://phabricator.endlessm.com/T33567

---
+++ packages/loading-screen/src/views/PermissionsExplanation.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="p-3 pb-5">
+      <h1 class="text-primary">
+        Permission
+      </h1>
+
+      <b-container class="inner-container">
+        <h5 class="font-weight-normal text-muted">
+          The Endless Key needs your Permission to access files on your device.
+        </h5>
+
+        <img class="mt-4 w-100" :src="permissionsImage">
+
+        <b-button
+          class="mt-5"
+          variant="primary"
+          @click="openSystemPermission"
+        >
+          Grant Permission
+        </b-button>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import PermissionsImage from '../assets/permissions-explanation.gif';
+
+  export default {
+    name: 'PermissionsExplanation',
+    data() {
+      return {
+        permissionsImage: PermissionsImage,
+      };
+    },
+    methods: {
+      openSystemPermission() {
+        this.$router.replace('/loading/default');
+        window.EndlessAPI.openSystemPermission();
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  @import '../styles';
+
+  // We need a custom container for the paragraph and the image:
+  .inner-container {
+    @include media-breakpoint-up(md) {
+      max-width: $inner-container-max-width;
+    }
+  }
+
+</style>
