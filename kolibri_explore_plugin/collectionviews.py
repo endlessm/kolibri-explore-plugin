b0b2a51888e7adf9ce2cddf9707c02954f33cad1
Manuel Quiñones <manuq@endlessos.org>
2022-10-18 19:25:33 -0300

    Add EndlessKeyContentManifest subclass
    Subclass Kolibri ContentManifest to add Endless Key logic:
    
    - Init with grade and name attributes. Use a filename convention to
      read the JSON manifest by grade and name.
    
    - Read custom metadata from the JSON manifest.
    
    - Set the available property using the free space as reported by
      Kolibri. Note that this check is a bit silly and using hardcoded
      number right now.
    
    Read all manifests when the module loads, and use a dict to organize
    them by grade and name.
    
    Finally implement the `get_collections_info()` GET API view to return
    the information serialized to the frontend.
    
    https://phabricator.endlessm.com/T33835

--- kolibri_explore_plugin/collectionviews.py
+++ kolibri_explore_plugin/collectionviews.py
@@ -1,12 +1,134 @@
+import logging
+import os
+
+from kolibri.core.content.utils.content_manifest import ContentManifest
+from kolibri.core.content.utils.content_manifest import (
+    ContentManifestParseError,
+)
+from kolibri.utils import conf
+from kolibri.utils.system import get_free_space
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+
+logger = logging.getLogger(__name__)
+
+COLLECTION_PATHS = os.path.join(
+    os.path.dirname(__file__), "static", "collections"
+)
+if conf.OPTIONS["Explore"]["CONTENT_COLLECTIONS_PATH"]:
+    COLLECTION_PATHS = conf.OPTIONS["Explore"]["CONTENT_COLLECTIONS_PATH"]
+
+COLLECTION_GRADES = ["primary", "intermediate", "secondary"]
+COLLECTION_NAMES = ["small", "large"]
+
+# FIXME: Move this metadata to the collections repo.
+GRADES_METADATA = {
+    "primary": {
+        "title": "for students ages 6-9",
+        "subtitle": "(in grades K-3)",
+    },
+    "intermediate": {
+        "title": "for students ages 10-13",
+        "subtitle": "(in grades 4-7)",
+    },
+    "secondary": {
+        "title": "for students ages 14+",
+        "subtitle": "(in grades 8+)",
+    },
+}
+
+
+class EndlessKeyContentManifest(ContentManifest):
+    def __init__(self, grade, name):
+        """The EK collections are organized by grade. Example: the
+        "primary-small" collection has grade "primary" and name
+        "small"
+        """
+        self.grade = grade
+        self.name = name
+        self.metadata = None
+        self.available = None
+        super().__init__()
+
+    def read(self, validate=False):
+        manifest_filename = os.path.join(
+            COLLECTION_PATHS, f"{self.grade}-{self.name}.json"
+        )
+
+        if not os.path.exists(manifest_filename):
+            raise ContentManifestParseError(
+                f"Collection manifest {manifest_filename} not found"
+            )
+
+        super().read(manifest_filename, validate)
+
+    def read_dict(self, manifest_data, validate=False):
+        self.metadata = manifest_data.get("metadata")
+        if self.metadata is None:
+            raise ContentManifestParseError(
+                "metadata is a required field for Endless Key manifest"
+            )
+        super().read_dict(manifest_data, validate)
+
+    def set_availability(self, free_space_gb):
+        # FIXME using a hardcoded number is silly. Find a way to get
+        # the exact weight.
+        if "required_gigabytes" in self.metadata:
+            self.available = (
+                self.metadata["required_gigabytes"] < free_space_gb
+            )
+        else:
+            self.available = False
+
+
+_content_manifests = []
+_content_manifests_by_grade_name = {}
+
+
+def _read_content_manifests():
+    global _content_manifests
+    global _content_manifests_by_grade_name
+
+    free_space_gb = get_free_space() / 1024**3
+
+    for grade in COLLECTION_GRADES:
+        for name in COLLECTION_NAMES:
+            manifest = EndlessKeyContentManifest(grade, name)
+            manifest.read(validate=True)
+            manifest.set_availability(free_space_gb)
+            _content_manifests.append(manifest)
+
+            if grade not in _content_manifests_by_grade_name:
+                _content_manifests_by_grade_name[grade] = {}
+            _content_manifests_by_grade_name[grade][name] = manifest
+
+
+_read_content_manifests()
 
 
 @api_view(["GET"])
 def get_collections_info(request):
     """Return the collections and their availability."""
-    # FIXME implement
-    return Response({"collectionsInfo": {}})
+    info = []
+    for grade in COLLECTION_GRADES:
+        grade_info = {
+            "grade": grade,
+            "collections": [],
+            "metadata": GRADES_METADATA[grade],
+        }
+        for name in COLLECTION_NAMES:
+            manifest = _content_manifests_by_grade_name[grade][name]
+            collection_info = {
+                "grade": manifest.grade,
+                "name": manifest.name,
+                "metadata": manifest.metadata,
+                "available": manifest.available,
+            }
+            grade_info["collections"].append(collection_info)
+        info.append(grade_info)
+
+    return Response({"collectionsInfo": info})
 
 
 @api_view(["GET"])
