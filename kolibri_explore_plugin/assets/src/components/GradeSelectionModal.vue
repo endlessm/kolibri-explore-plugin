86e5096cf0db9e0bebf9c3a43bad9dbe67c17d55
Manuel Quiñones <manuq@endlessos.org>
2022-10-21 17:16:41 -0300

    Remove extra div from GradeSelectionModal
    Doing this in a separate commit to have better diffs in next commits.
    
    https://phabricator.endlessm.com/T33835

--- kolibri_explore_plugin/assets/src/components/GradeSelectionModal.vue
+++ kolibri_explore_plugin/assets/src/components/GradeSelectionModal.vue
@@ -1,57 +1,55 @@
 <template>
 
-  <div>
-    <b-modal
-      id="grade-selection-modal"
-      size="xl"
-      centered
-      :visible="true"
-      :noCloseOnBackdrop="true"
-      :noCloseOnEsc="true"
-      :hideFooter="true"
-      :hideHeaderClose="true"
-    >
-      <b-container>
-        <h1 class="text-primary">
-          Select grade range
-        </h1>
+  <b-modal
+    id="grade-selection-modal"
+    size="xl"
+    centered
+    :visible="true"
+    :noCloseOnBackdrop="true"
+    :noCloseOnEsc="true"
+    :hideFooter="true"
+    :hideHeaderClose="true"
+  >
+    <b-container>
+      <h1 class="text-primary">
+        Select grade range
+      </h1>
 
-        <b-alert
-          v-if="error"
-          variant="danger"
-          show
-          fade
+      <b-alert
+        v-if="error"
+        variant="danger"
+        show
+        fade
+      >
+        {{ error }}
+      </b-alert>
+
+      <b-card-group deck class="py-5">
+        <WelcomeCard
+          v-for="grade in grades"
+          :key="grade.key"
+          :title="grade.key"
+          :text="grade.text"
+          :secondaryText="grade.secondaryText"
+          :selected="selection === grade.key"
+          @click="setSelection(grade.key)"
         >
-          {{ error }}
-        </b-alert>
+          <img :src="grade.image" width="100%">
+        </WelcomeCard>
+      </b-card-group>
+      <div class="pb-3 pt-3">
+        <b-button
+          class="m-1"
+          :disabled="selection === null"
+          variant="primary"
+          @click="onNext"
+        >
+          Next
+        </b-button>
+      </div>
 
-        <b-card-group deck class="py-5">
-          <WelcomeCard
-            v-for="grade in grades"
-            :key="grade.key"
-            :title="grade.key"
-            :text="grade.text"
-            :secondaryText="grade.secondaryText"
-            :selected="selection === grade.key"
-            @click="setSelection(grade.key)"
-          >
-            <img :src="grade.image" width="100%">
-          </WelcomeCard>
-        </b-card-group>
-        <div class="pb-3 pt-3">
-          <b-button
-            class="m-1"
-            :disabled="selection === null"
-            variant="primary"
-            @click="onNext"
-          >
-            Next
-          </b-button>
-        </div>
-
-      </b-container>
-    </b-modal>
-  </div>
+    </b-container>
+  </b-modal>
 
 </template>
 
