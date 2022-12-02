627597d0f5a26288d2dfbf2c96f99bab02a5f0a1
Manuel Quiñones <manuq@endlessos.org>
2022-12-02 08:17:46 -0300

    Welcome screen: Add modal with privacy policy
    In the welcome / get started view. Add a link that pops up a modal with
    the text.
    
    The modal is themed in the same way as other modals using the awkward
    deep selector for vue SASS.
    
    Note that we are not marking labels as translatable in the welcome
    screen yet. This is a standalone webapp, not a kolibri plugin.
    
    https://phabricator.endlessm.com/T33905

--- packages/loading-screen/src/views/Welcome.vue
+++ packages/loading-screen/src/views/Welcome.vue
@@ -8,7 +8,7 @@
       <h5 class="font-weight-normal text-muted">
         Let's begin by helping you find your content.
       </h5>
-      <div class="pt-5">
+      <div class="pb-4 pt-5">
         <b-button
           variant="primary"
           to="select-source"
@@ -16,17 +16,41 @@
           Get Started
         </b-button>
       </div>
+      <p class="font-weight-normal text-muted">
+        Check our
+        <b-link
+          v-b-modal.privacy-policy-modal
+        >
+          Privacy Policy
+        </b-link>.
+      </p>
     </div>
     <div class="flex-grow-1 welcome-background-bottom"></div>
+    <b-modal
+      id="privacy-policy-modal"
+      headerCloseVariant="light"
+      size="xl"
+      centered
+    >
+      <div class="bg-white modal-wrapper text-dark">
+        <h3 class="pt-3 text-primary">
+          Privacy Policy
+        </h3>
+        <PrivacyPolicyText class="pb-3" />
+      </div>
+    </b-modal>
   </div>
 </template>
 
 
 <script>
+import PrivacyPolicyText from 'eos-components/src/components/PrivacyPolicyText.vue';
+
 
   export default {
-    name: 'Welcome',
-  };
+    name: "Welcome",
+    components: { PrivacyPolicyText }
+};
 
 </script>
 
@@ -49,6 +73,23 @@
   .welcome-background-bottom {
     background-image: url('../assets/welcome-bg-bottom-left.jpg');
     background-position: bottom left;
+  }
+
+  .modal-wrapper {
+    padding-right: $spacer;
+    padding-left: $spacer;
+    @include media-breakpoint-up(md) {
+      padding-right: $grid-gutter-width + $circled-button-size;
+      padding-left: $grid-gutter-width + $circled-button-size;
+    }
+  }
+
+  ::v-deep .modal-body {
+    padding-top: 0;
+  }
+
+  ::v-deep .modal-footer {
+    display: none;
   }
 
 </style>
