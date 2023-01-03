3a9646d4748a7260c24788c20238d7dbe34116e9
Manuel Quiñones <manuq@endlessos.org>
2023-01-03 12:37:13 -0300

    fixup! Backend: Add models for externally tagged content

--- kolibri_explore_plugin/models.py
+++ kolibri_explore_plugin/models.py
@@ -5,21 +5,12 @@ from morango.models import UUIDField
 
 class ExternalContentTag(models.Model):
     id = UUIDField(primary_key=True)
-    tag_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.tag_name
-
-
-class TaggedContent(models.Model):
-    content_node = models.OneToOneField(
-        ContentNode, on_delete=models.CASCADE, primary_key=True
-    )
-    tags = models.ManyToManyField(
-        ExternalContentTag,
+    tag_name = models.CharField(max_length=100, unique=True)
+    content_nodes = models.ManyToManyField(
+        ContentNode,
         symmetrical=False,
         related_name="tagged_content",
     )
 
     def __str__(self):
-        return self.content_node.title
+        return self.tag_name
