fb8e315334d2c619d5ddf7e38e9e6a9b6f569034
Manuel Quiñones <manuq@endlessos.org>
2023-01-06 15:26:27 -0300

    Collections: Query backend models from frontend
    Add viewsets for fetching external tags and content nodes matching them.
    
    And add the viewsets to the API router. Finally, define frontend
    resources to query the API.
    
    Example usage:
    
        // Print all external tags
        ExternalContentTagResource.fetchCollection()
        .then(tags => {
          console.log(tags);
        });
    
        // Print content nodes matching "highlight"
        ContentNodeExtrasResource.fetchByExternalTag('highlight')
        .then(nodes => {
          console.log(nodes);
        });
    
    https://phabricator.endlessm.com/T34285

---
+++ kolibri_explore_plugin/assets/src/apiResources.js
@@ -0,0 +1,14 @@
+import { Resource } from 'kolibri.lib.apiResource';
+
+export const ExternalContentTagResource = new Resource({
+  name: 'externalcontenttag',
+  namespace: 'kolibri_explore_plugin',
+});
+
+export const ContentNodeExtrasResource = new Resource({
+  name: 'contentnodeextras',
+  namespace: 'kolibri_explore_plugin',
+  fetchByExternalTag(tag) {
+    return this.getListEndpoint('by_external_tag', { tag });
+  },
+});
