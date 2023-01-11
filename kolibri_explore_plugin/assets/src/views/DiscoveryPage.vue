f0849de340da2afca9096693e802a99f42d08362
Manuel Quiñones <manuq@endlessos.org>
2023-01-11 16:58:18 -0300

    Discovery: Add a feature flag for the new collections
    As a new option for the plugin, which can be passed in Kolibri
    options.ini as:
    
        [Explore]
        NEW_COLLECTIONS_PAGE_FEATURE = True
    
    And read it from the frontend to conditionally compose either the
    previous view or the new one in development.
    
    https://phabricator.endlessm.com/T34285

--- kolibri_explore_plugin/assets/src/views/DiscoveryPage.vue
+++ kolibri_explore_plugin/assets/src/views/DiscoveryPage.vue
@@ -7,6 +7,7 @@
 
 <script>
 
+  import plugin_data from 'plugin_data';
   import DiscoveryPageCollections from './DiscoveryPageCollections';
   import DiscoveryPageFullContent from './DiscoveryPageFullContent';
 
@@ -15,8 +16,8 @@
     components: { DiscoveryPageCollections, DiscoveryPageFullContent },
     computed: {
       showCollectionsPage() {
-        // FIXME read this from a setting.
-        return false;
+        // FIXME return false if "use EK USB" was selected in the onboarding.
+        return plugin_data.newCollectionsPageFeature;
       },
       pageVariant() {
         if (this.showCollectionsPage) {
