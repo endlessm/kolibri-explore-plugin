e35e2d38de4dfebdac2412ba095eceea4723b890
Manuel Quiñones <manuq@endlessos.org>
2023-02-06 15:51:58 -0300

    Bump yarn to latest
    To avoid the "yarn is out of date" warning.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,6 @@
 
 pipenv install
 pipenv run nodeenv -p --node=16.14.0
-pipenv run npm install -g yarn@1.22.10
+pipenv run npm install -g yarn@1.22.19
 pipenv run yarn global add @vue/cli@v4.5.15
 pipenv run yarn install --ignore-engines
