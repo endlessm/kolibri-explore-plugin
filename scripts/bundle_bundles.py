f64d7d35b0506d11c0bc47f09c58b040d2d8ca63
Manuel Quiñones <manuq@endlessos.org>
2023-02-28 15:55:34 -0300

    Remove kolibri-api package
    The last API has been ported so there is no need for this package
    anymore.
    
    https://phabricator.endlessm.com/T34521

--- scripts/bundle_bundles.py
+++ scripts/bundle_bundles.py
@@ -11,7 +11,7 @@ from _common import get_available_overrides
 from _common import TEMPLATE_WORKSPACE
 
 DEFAULT_ZIP_FILENAME = "custom-channel-ui.zip"
-SKIP_WORKSPACES = ["kolibri-api", "welcome-screen", TEMPLATE_WORKSPACE]
+SKIP_WORKSPACES = ["welcome-screen", TEMPLATE_WORKSPACE]
 
 
 def validate_dir_path(path):
