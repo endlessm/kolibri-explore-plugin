ab17d3b78b31c0046c56a818b363fce42f773f83
Manuel Quiñones <manuq@endlessos.org>
2023-04-10 10:33:28 -0300

    Revert "Frontend download: Set is US user before download"
    This reverts commit 9eb7990df26caed8b3c6addffc5d92abe0c2d501.

--- kolibri_explore_plugin/assets/src/modules/coreExplore/utils.js
+++ kolibri_explore_plugin/assets/src/modules/coreExplore/utils.js
@@ -108,16 +108,8 @@ export function cancelDownload() {
   });
 }
 
-function _isUsUser() {
+export function isUsUser() {
   const allUsTimezones = new Intl.Locale('en-US').timeZones;
   const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   return allUsTimezones.includes(userTimezone);
-}
-
-export function setIsUsUser() {
-  return client({
-    url: urls['kolibri:kolibri_explore_plugin:set_is_us_user'](),
-    method: 'POST',
-    data: { is_us_user: _isUsUser() },
-  });
 }
