47368fa130b8e76862e585d60adcc8af512cb664
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-06-07 12:01:21 -0300

    SectionsSearchRow: Move button group to a column
    This has no practical effect, but will simplify next patch.
    
    Move the button group to a row > column.

--- packages/template-ui/src/components/SectionsSearchRow.vue
+++ packages/template-ui/src/components/SectionsSearchRow.vue
@@ -1,16 +1,23 @@
 <template>
   <b-container class="my-4">
-    <MainSections>
-      <b-button
-        class="font-weight-bold my-1 text-nowrap"
-        size="sm"
-        variant="link"
-        to="/search"
-      >
-        <MagnifyIcon :size="iconSize" class="mr-1" />
-        Search Keywords
-      </b-button>
-    </MainSections>
+    <b-row>
+
+      <!-- Section buttons -->
+      <b-col>
+        <MainSections>
+          <b-button
+            class="font-weight-bold my-1 text-nowrap"
+            size="sm"
+            variant="link"
+            to="/search"
+          >
+            <MagnifyIcon :size="iconSize" class="mr-1" />
+            Search Keywords
+          </b-button>
+        </MainSections>
+      </b-col>
+
+    </b-row>
   </b-container>
 </template>
 
