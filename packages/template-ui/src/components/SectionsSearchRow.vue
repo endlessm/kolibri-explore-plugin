a2d72bfe3b8e13227846293299954bf64f5cf984
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-06-07 12:02:46 -0300

    SectionsSearchRow: Add Download button
    This adds a stub download button to the SectionsSearchRow
    component. The button is only visible when the channel is
    not available.
    
    It doesn't download anything right now, that's stub.
    
    See https://github.com/endlessm/kolibri-explore-plugin/issues/532

--- packages/template-ui/src/components/SectionsSearchRow.vue
+++ packages/template-ui/src/components/SectionsSearchRow.vue
@@ -17,20 +17,34 @@
         </MainSections>
       </b-col>
 
+      <!-- Download button -->
+      <b-col v-if="showDownloadButton" md="auto">
+        <b-button pill class="font-weight-bold text-nowrap" variant="outline-dark">
+          <CloudDownloadOutlineIcon :size="iconSize" class="mr-1" />
+          Download full channel
+        </b-button>
+      </b-col>
+
     </b-row>
   </b-container>
 </template>
 
 <script>
+import CloudDownloadOutlineIcon from 'vue-material-design-icons/CloudDownloadOutline.vue';
 import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
 import { constants } from 'ek-components';
+import { mapState } from 'vuex';
 
 export default {
   name: 'SectionsSearchRow',
-  components: { MagnifyIcon },
+  components: { CloudDownloadOutlineIcon, MagnifyIcon },
   computed: {
+    ...mapState(['channel']),
     iconSize() {
       return constants.KeywordIconSize;
+    },
+    showDownloadButton() {
+      return !this.channel.available;
     },
   },
 };
