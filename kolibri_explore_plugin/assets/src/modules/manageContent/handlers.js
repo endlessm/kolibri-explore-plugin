8795f44621e421b5c006585504b682b5cde2d9db
Manuel Quiñones <manuq@endlessos.org>
2023-06-13 20:53:24 -0300

    Add new manageContent plugin submodule
    This is a Vuex module that stores the content download tasks indexed
    by channel ID and content ID.
    
    There are mutations to set all at once (not yet used) and to set the
    download task for a particular channel and content IDs.
    
    There is a getter to obtain the task, given the channel and content
    IDs.
    
    Finally, there are handlers to implement the content download start,
    check, and retry. In there the Kolibri task status is mapped to a
    download state that the UI understands.

---
+++ kolibri_explore_plugin/assets/src/modules/manageContent/handlers.js
@@ -0,0 +1,78 @@
+import { TaskStatuses, TaskTypes } from 'kolibri.utils.syncTaskUtils';
+import { constants } from 'ek-components';
+import { TaskResource } from 'kolibri.resources';
+import store from 'kolibri.coreVue.vuex.store';
+
+function _mapTaskStatusToDownloadState(task) {
+  switch (task.status) {
+    case TaskStatuses.CANCELED:
+      return constants.DownloadState.READY;
+    case TaskStatuses.IN_PROGRESS:
+    case TaskStatuses.PENDING:
+    case TaskStatuses.RUNNING:
+    case TaskStatuses.QUEUED:
+    case TaskStatuses.SCHEDULED:
+    case TaskStatuses.CANCELLING:
+      return constants.DownloadState.DOWNLOADING;
+    case TaskStatuses.COMPLETED:
+      return constants.DownloadState.COMPLETED;
+    case TaskStatuses.FAILED:
+      return constants.DownloadState.FAILED;
+    default:
+      return constants.DownloadState.NOT_CHECKED;
+  }
+}
+
+export function checkContentDownload(channelId, contentId) {
+  // Check if task ID is in the store:
+  const task = store.getters['manageContent/getContentDownloadTask'](channelId, contentId);
+  // If not, the resource is ready for downloading:
+  if (task === undefined) {
+    return new Promise(resolve => {
+      resolve(constants.DownloadState.READY);
+    });
+  }
+  // Else, check how the task is going and update the task in the store:
+  return TaskResource.get(task.id).then(newTask => {
+    store.commit('manageContent/SET_CONTENT_DOWNLOAD_TASK', {
+      channelId,
+      contentId,
+      task: newTask,
+    });
+    return _mapTaskStatusToDownloadState(newTask);
+  });
+}
+
+export function startContentDownload(channelId, contentId) {
+  const taskParams = {
+    channel_id: channelId,
+    channel_name: 'foo',
+    node_ids: [contentId],
+    type: TaskTypes.REMOTECONTENTIMPORT,
+  };
+
+  return TaskResource.startTask(taskParams).then(task => {
+    store.commit('manageContent/SET_CONTENT_DOWNLOAD_TASK', { channelId, contentId, task });
+    return _mapTaskStatusToDownloadState(task);
+  });
+}
+
+export function retryContentDownload(channelId, contentId) {
+  // Check if task ID is in the store:
+  const task = store.getters['manageContent/getContentDownloadTask'](channelId, contentId);
+  // If not, the resource is ready for downloading:
+  if (task === undefined) {
+    return new Promise(resolve => {
+      resolve(constants.DownloadState.READY);
+    });
+  }
+  // Else, restart the task and update the task in the store:
+  return TaskResource.restart(task.id).then(newTask => {
+    store.commit('manageContent/SET_CONTENT_DOWNLOAD_TASK', {
+      channelId,
+      contentId,
+      task: newTask,
+    });
+    return _mapTaskStatusToDownloadState(newTask);
+  });
+}
