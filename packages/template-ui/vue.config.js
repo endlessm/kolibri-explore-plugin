ff287632cfebab543012d84127b524d334a32673
Manuel Quiñones <manuq@endlessos.org>
2023-06-26 19:16:59 -0300

    Remove vue-clamp dependency

--- packages/template-ui/vue.config.js
+++ packages/template-ui/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   publicPath: './',
-  transpileDependencies: ['vue-clamp', 'resize-detector'],
   css: {
     // FIXME this is because of IE11 compatibility:
     extract: false,
