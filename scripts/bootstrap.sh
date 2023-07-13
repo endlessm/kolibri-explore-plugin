fdc883309bd77ff78e343b7d000034c5058c8df1
Philip Withnall <pwithnall@endlessos.org>
2023-07-13 13:14:19 +0100

    ci: Add --ci argument to bootstrap.sh and use it from CI jobs
    Passing `--ci` will cause `--ignore-pipfile` to be passed to `pipenv
    install`, which will cause it to only use the `Pipfile.lock` for
    installing dependencies, and not install the latest versions which match
    `Pipfile`, or to update the `Pipfile.lock`. This means that the
    dependency versions used in CI are properly pinned, and hence we’re not
    testing them as well as whatever changes are in the associated PR at the
    same time.
    
    `bootstrap.sh` unfortunately can’t be used from `node.yml` because it
    benefits from using `actions/setup-node` to cache the node installation.
    
    Prompted by https://github.com/endlessm/kolibri-explore-plugin/pull/701
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,29 @@
 #
 # Setup Project
 
-pipenv install --dev --deploy
+# Argument parsing
+pipenv_install_args=
+
+while :; do
+  case $1 in
+    --ci)
+      # Prevent the Pipfile.lock from being updated automatically. Just use
+      # the pinned dependencies.
+      pipenv_install_args=( "--ignore-pipfile" )
+      ;;
+    -h|-\?|--help|-?*)
+      echo "Usage: bootstrap.sh [--ci]" >&2
+      exit
+      ;;
+    *)
+      break
+  esac
+
+  shift
+done
+
+# Install things
+pipenv install --dev --deploy "${pipenv_install_args[@]}"
 pipenv run nodeenv -p --node=16.14.0
 pipenv run npm install -g yarn@1.22.19
 pipenv run yarn global add @vue/cli@v4.5.15
