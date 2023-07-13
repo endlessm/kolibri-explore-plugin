61c712a1427bf88c5a73c62206dafd68e3d744f2
Jian-Hong Pan <jhp@endlessos.org>
2023-07-13 09:35:52 +0000

    bootstrap: Using pipenv for Deployments and avoid re-lock Pipfile
    According to Pipfile.lock Security Features [1] and Using pipenv for
    Deployments [2], enable --deploy option for "pipenv install" to check
    the Pipfile.lock is up to date, or not. It will return failed if the
    Pipfile.lock is out of date, instead of generating a new one.
    
    [1]: https://pipenv.pypa.io/en/latest/pipfile/#pipfile-lock-security-features
    [2]: https://pipenv-fork.readthedocs.io/en/latest/advanced.html#using-pipenv-for-deployments

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 #
 # Setup Project
 
-pipenv install --dev
+pipenv install --dev --deploy
 pipenv run nodeenv -p --node=16.14.0
 pipenv run npm install -g yarn@1.22.19
 pipenv run yarn global add @vue/cli@v4.5.15
