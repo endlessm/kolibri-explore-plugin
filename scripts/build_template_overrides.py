68b6ffebe3b5c8ac95a47af8d02e87af3cdc1477
Dan Nicholson <dbn@endlessos.org>
2023-09-09 08:33:04 -0600

    Only use LZMA compression for channel template zips
    Having `bundle_zip` default to LZMA compression meant it also applied to
    `welcome-screen.zip`. That's problematic since `unzip` can't handle that
    compression scheme. It's also unnecessary as `welcome-screen.zip` is
    only intended for releases. All of our apps will expand it into some
    location in their package, so the compression used is irrelevant.
    
    Instead, make the default zlib deflate compression and explicitly
    request LZMA compression just for the channel templates. Note that the
    outer `apps-bundle.zip` is actually created with no compression, which
    is fine since it's entirely for distributing the already compressed
    channel templates.
    
    See https://github.com/endlessm/endless-key-flatpak/issues/26.

--- scripts/build_template_overrides.py
+++ scripts/build_template_overrides.py
@@ -2,6 +2,7 @@
 # Copyright 2021 Endless OS Foundation LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 import subprocess
+import zipfile
 
 from _common import bundle_zip
 from _common import get_available_overrides
@@ -10,4 +11,4 @@ from _common import set_channel_override
 for override in get_available_overrides():
     set_channel_override(override)
     subprocess.run(["vue-cli-service", "build"])
-    bundle_zip(override)
+    bundle_zip(override, compression=zipfile.ZIP_LZMA)
