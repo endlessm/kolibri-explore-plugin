375d34e2db5d0ef3a0194ad9cf3edb5c50cae8b9
Manuel Quiñones <manuq@endlessos.org>
2023-09-12 14:42:47 -0300

    Migrate Welcome pages to the plugin
    Move the welcome and pack selection/confirmation pages from
    the dedicated package to the plugin. Adapt the code to the plugin
    linter. Adapt the links to point to the corresponding routes. Move the
    graphic assets along.

--- kolibri_explore_plugin/assets/src/views/welcome/PackReadyPage.vue
+++ kolibri_explore_plugin/assets/src/views/welcome/PackReadyPage.vue
@@ -1,5 +1,86 @@
 <template>
 
-  <h1>Ready!</h1>
+  <WelcomeBase title="Awesome! Your Starter Pack is ready">
+    <template #body>
+      <div class="pack-background"></div>
+      <b-button
+        pill
+        class="mb-4 mt-4"
+        :disabled="isOffline"
+        variant="primary"
+        @click="downloadContent"
+      >
+        Download My Starter Pack
+      </b-button>
+      <p>
+        <b-link
+          :to="{ name: PageNames.WELCOME_PACK_SELECTION }"
+        >
+          Not sure? Let's go back to all the options.
+        </b-link>
+      </p>
+    </template>
+  </WelcomeBase>
 
 </template>
+
+
+<script>
+
+  import { PageNames } from '../../constants';
+  import WelcomeBase from './WelcomeBase';
+
+  export default {
+    name: 'PackReadyPage',
+    components: {
+      WelcomeBase,
+    },
+    data() {
+      return {
+        isOffline: false,
+        PageNames,
+      };
+    },
+    created() {
+      this.isOffline = !navigator.onLine;
+      window.addEventListener('offline', this.onOffline);
+      window.addEventListener('online', this.onOnline);
+    },
+    destroyed() {
+      window.removeEventListener('offline', this.onOffline);
+      window.removeEventListener('online', this.onOnline);
+    },
+    methods: {
+      downloadContent() {
+        const grade = this.$route.params.grade;
+        const name = this.$route.params.name;
+        this.$router.push({ name: PageNames.DOWNLOAD, params: { grade, name } });
+      },
+      onOffline() {
+        this.isOffline = true;
+      },
+      onOnline() {
+        this.isOffline = false;
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  @import '../../styles';
+
+  .pack-background {
+    height: 170px;
+    background-image: url('assets/pack.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    @include media-breakpoint-down(sm) {
+      height: 120px;
+    }
+  }
+
+</style>
--- kolibri_explore_plugin/assets/src/views/welcome/WelcomePage.vue
+++ kolibri_explore_plugin/assets/src/views/welcome/WelcomePage.vue
@@ -1,5 +1,106 @@
 <template>
 
-  <h1>Welcome</h1>
+  <div
+    class="d-flex main-container min-vh-100 text-center"
+  >
+    <div class="d-flex flex-column flex-grow-1 w-100">
+      <div class="flex-grow-1 welcome-background-top"></div>
+      <div class="content p-3 pb-5 position-relative">
+        <h1>
+          Welcome to Endless Key!
+        </h1>
+        <div class="mb-4 mt-4">
+          <b-button
+            pill
+            variant="primary"
+            :to="{ name: PageNames.WELCOME_PACK_SELECTION }"
+          >
+            Get Started
+          </b-button>
+        </div>
+        <p>
+          <b-link v-b-modal.privacy-policy-modal>
+            Privacy Policy
+          </b-link>
+        </p>
+      </div>
+      <div class="flex-grow-1 welcome-background-bottom"></div>
+      <b-modal
+        id="privacy-policy-modal"
+        headerCloseVariant="light"
+        size="xl"
+        centered
+      >
+        <div class="bg-white modal-wrapper text-dark">
+          <h3 class="pt-3 text-primary">
+            Privacy Policy
+          </h3>
+          <EkPrivacyPolicyText class="pb-3" />
+        </div>
+      </b-modal>
+    </div>
+  </div>
 
 </template>
+
+
+<script>
+
+  import EkPrivacyPolicyText from 'ek-components/src/components/EkPrivacyPolicyText.vue';
+  import { PageNames } from '../../constants';
+
+  export default {
+    name: 'WelcomePage',
+    components: { EkPrivacyPolicyText },
+    data() {
+      return {
+        PageNames,
+      };
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped>
+
+  @import '../../styles';
+
+  .main-container {
+    background-color: white;
+  }
+
+  .welcome-background-bottom,
+  .welcome-background-top {
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+
+  .welcome-background-top {
+    background-image: url('assets/welcome-bg-top-right.png');
+    background-position: top right;
+  }
+
+  .welcome-background-bottom {
+    background-image: url('assets/welcome-bg-bottom-left.png');
+    background-position: bottom left;
+  }
+
+  .modal-wrapper {
+    padding-right: $spacer;
+    padding-left: $spacer;
+    @include media-breakpoint-up(md) {
+      padding-right: $grid-gutter-width + $circled-button-size;
+      padding-left: $grid-gutter-width + $circled-button-size;
+    }
+  }
+
+  ::v-deep .modal-body {
+    padding-top: 0;
+  }
+
+  ::v-deep .modal-footer {
+    display: none;
+  }
+
+</style>
