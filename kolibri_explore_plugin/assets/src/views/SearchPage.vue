endlessm/kolibri-explore-plugin

commit 075a001cc0898a6567385e1876616741ec3c3746

ek-components: Translate various string constants
These need to be translated using a separate translator, since they are
in a standalone JavaScript file rather than a vue template.

The code requires `kolibri.utils.i18n` now, so kolibri has to be added
as a dependency.

Signed-off-by: Philip Withnall <philip@tecnocode.co.uk>

Helps: #771

kolibri_explore_plugin/assets/src/views/SearchPage.vue
@@ -283,7 +283,7 @@
         this.query = '';
       },
       groupVerb(kind) {
-        return constants.MediaTypeVerbs[kind === 'topic' ? 'bundle' : kind];
+        return constants.mediaTypeVerb[kind === 'topic' ? 'bundle' : kind];
       },
       removeKeyword(keyword) {
         const words = this.keywords.filter(k => k !== keyword);

kolibri_explore_plugin/assets/src/views/welcome/PackSelectionPage.vue
@@ -17,8 +17,8 @@
             <EkPackCard
               :key="pack.id"
               :packId="pack.id"
-              :title="pack.title"
-              :subTitle="pack.subtitle"
+              :title="packMetadataTitle(pack)"
+              :subTitle="packMetadataSubtitle(pack)"
               @click="choosePack(pack.id)"
             />
           </template>
@@ -32,7 +32,11 @@
 
 <script>
 
-  import { PackMetadata } from 'ek-components/src/constants';
+  import {
+    PackMetadata,
+    packMetadataTitle,
+    packMetadataSubtitle,
+  } from 'ek-components/src/constants';
   import { PageNames } from '../../constants';
   import WelcomeBase from './WelcomeBase';
 
@@ -47,6 +51,8 @@
       };
     },
     methods: {
+      packMetadataTitle,
+      packMetadataSubtitle,
       choosePack(grade) {
         // FIXME: This is hardcoded to the (currently) only option.
         const name = '0001';

packages/ek-components/src/components/EkPlayButton.vue
@@ -25,7 +25,7 @@ import ExerciseIcon from 'vue-material-design-icons/CheckboxMarkedCircleOutline.
 import Html5Icon from 'vue-material-design-icons/MotionOutline.vue';
 import BundleIcon from 'vue-material-design-icons/CubeOutline.vue';
 import VideoIcon from 'vue-material-design-icons/PlayCircleOutline.vue';
-import { MediaTypeVerbs } from '../constants';
+import { MediaTypeVerbs, mediaTypeVerb } from '../constants';
 
 export default {
   name: 'EkPlayButton',
@@ -100,7 +100,10 @@ export default {
       if (this.label) {
         return this.label;
       }
-      return (MediaTypeVerbs[this.kind] !== null && MediaTypeVerbs[this.kind] !== undefined) ? MediaTypeVerbs[this.kind] : MediaTypeVerbs['video'];
+      if (this.kind in MediaTypeVerbs) {
+        return mediaTypeVerb(this.kind);
+      }
+      return mediaTypeVerb('video');
     },
   },
 };

packages/ek-components/src/constants.js
@@ -1,15 +1,105 @@
+const createTranslator = window.kolibri.createTranslator;
+
+// We have to call something called exactly ‘createTranslator’ for
+// i18n-extract-messages to work. See the documentation in kolibriApi.js.
+export const ekComponentsConstantsStrings = createTranslator('EkComponentsConstants', {
+  // For MediaTypeVerbs
+  discoverVerb: {
+    message: 'Discover',
+    context: 'Verb for using a media type',
+  },
+  watchVerb: {
+    message: 'Watch',
+    context: 'Verb for using a media type',
+  },
+  listenVerb: {
+    message: 'Listen',
+    context: 'Verb for using a media type',
+  },
+  readVerb: {
+    message: 'Read',
+    context: 'Verb for using a media type',
+  },
+  practiceVerb: {
+    message: 'Practice',
+    context: 'Verb for using a media type',
+  },
+  interactVerb: {
+    message: 'Interact',
+    context: 'Verb for using a media type',
+  },
+
+  // For PackMetadata
+  explorerTitle: {
+    message: 'Explorer',
+    context: 'Title of a content pack',
+  },
+  explorerSubtitle: {
+    message: 'I like to learn about different cultures, places and ideas.',
+    context: 'Subtitle of a content pack',
+  },
+  artistTitle: {
+    message: 'Artist',
+    context: 'Title of a content pack',
+  },
+  artistSubtitle: {
+    message: 'I am creative and enjoy making things, music and dancing.',
+    context: 'Subtitle of a content pack',
+  },
+  scientistTitle: {
+    message: 'Scientist',
+    context: 'Title of a content pack',
+  },
+  scientistSubtitle: {
+    message: 'I love to investigate the world and do fun experiments.',
+    context: 'Subtitle of a content pack',
+  },
+  inventorTitle: {
+    message: 'Inventor',
+    context: 'Title of a content pack',
+  },
+  inventorSubtitle: {
+    message: 'I like to build things and solve problems.',
+    context: 'Subtitle of a content pack',
+  },
+  athleteTitle: {
+    message: 'Athlete',
+    context: 'Title of a content pack',
+  },
+  athleteSubtitle: {
+    message: 'I like to move my body and be with friends.',
+    context: 'Subtitle of a content pack',
+  },
+  curiousTitle: {
+    message: 'Curious',
+    context: 'Title of a content pack',
+  },
+  curiousSubtitle: {
+    message: 'I like to experiment with a bit of everything.',
+    context: 'Subtitle of a content pack',
+  },
+});
+
+// This maps to a translation ID. Use mediaTypeVerb() to translate it.
 export const MediaTypeVerbs = {
-  'bundle': 'Discover',
-  'video': 'Watch',
-  'audio': 'Listen',
-  'document': 'Read',
-  'exercise': 'Practice',
-  'html5': 'Interact',
-  'zim': 'Interact',
-  'slideshow': 'Interact',
-  'h5p': 'Interact',
-  'quiz': 'Interact',
+  'bundle': 'discoverVerb',
+  'video': 'watchVerb',
+  'audio': 'listenVerb',
+  'document': 'readVerb',
+  'exercise': 'practiceVerb',
+  'html5': 'interactVerb',
+  'zim': 'interactVerb',
+  'slideshow': 'interactVerb',
+  'h5p': 'interactVerb',
+  'quiz': 'interactVerb',
 };
+
+export function mediaTypeVerb(id) {
+  if (id in MediaTypeVerbs)
+    return ekComponentsConstantsStrings.$tr(MediaTypeVerbs[id]);
+  else
+    return null;
+}
 
 export const MediaFilterName = 'Learning activity';
 export const AuthorFilterName = 'author';
@@ -70,35 +160,43 @@ export const CollectionsSections = ['highlight', 'skill', 'career', 'curious'];
 export const PackMetadata = [
     {
         id: 'explorer',
-        title: 'Explorer',
-        subtitle: 'I like to learn about different cultures, places and ideas.',
+        titleId: 'explorerTitle',
+        subtitleId: 'explorerSubtitle',
     },
     {
         id: 'artist',
-        title: 'Artist',
-        subtitle: 'I am creative and enjoy making things, music and dancing.',
+        titleId: 'artistTitle',
+        subtitleId: 'artistSubtitle',
     },
     {
         id: 'scientist',
-        title: 'Scientist',
-        subtitle: 'I love to investigate the world and do fun experiments.',
+        titleId: 'scientistTitle',
+        subtitleId: 'scientistSubtitle',
     },
     {
         id: 'inventor',
-        title: 'Inventor',
-        subtitle: 'I like to build things and solve problems.',
+        titleId: 'inventorTitle',
+        subtitleId: 'inventorSubtitle',
     },
     {
         id: 'athlete',
-        title: 'Athlete',
-        subtitle: 'I like to move my body and be with friends.',
+        titleId: 'athleteTitle',
+        subtitleId: 'athleteSubtitle',
     },
     {
         id: 'curious',
-        title: 'Curious',
-        subtitle: 'I like to experiment a bit of everything.',
+        titleId: 'curiousTitle',
+        subtitleId: 'curiousSubtitle',
     },
 ];
+
+export function packMetadataTitle(pack) {
+  return ekComponentsConstantsStrings.$tr(pack.titleId);
+}
+
+export function packMetadataSubtitle(pack) {
+  return ekComponentsConstantsStrings.$tr(pack.subtitleId);
+}
 
 export default {
   AuthorFilterName,
@@ -114,7 +212,10 @@ export default {
   MediaFilterName,
   MediaQuality,
   MediaTypeVerbs,
+  mediaTypeVerb,
   PackMetadata,
+  packMetadataTitle,
+  packMetadataSubtitle,
   SearchBarDebounce,
   StructuredTags,
   StructuredTagsRegExp,

packages/template-ui/src/components/FilterContent.vue
@@ -133,7 +133,7 @@ export default {
       }
     },
     contentKindToVerb(value) {
-      return constants.MediaTypeVerbs[value] || value;
+      return constants.mediaTypeVerb(value) || value;
     },
     sortOptionsByWeight(weightedOptions) {
       return Object.keys(weightedOptions).sort((a, b) => weightedOptions[b] - weightedOptions[a]);

packages/template-ui/src/store/modules/filters.js
@@ -27,7 +27,7 @@ const ContentNodeKinds = [
 ];
 
 function contentKindToVerb(value) {
-  return constants.MediaTypeVerbs[value] || value;
+  return constants.mediaTypeVerb(value) || value;
 }
 
 const {

commit a3c2efc896e2f1ea5afa67d3e76574c1fbc55d9c

app: Fix typo in function call in SearchPage
This was introduced in 075a001cc0898a6567385e1876616741ec3c3746 when
`MediaTypeVerbs[]` was replaced by `mediaTypeVerb()`.

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

kolibri_explore_plugin/assets/src/views/SearchPage.vue
@@ -283,7 +283,7 @@
         this.query = '';
       },
       groupVerb(kind) {
-        return constants.mediaTypeVerb[kind === 'topic' ? 'bundle' : kind];
+        return constants.mediaTypeVerb(kind === 'topic' ? 'bundle' : kind);
       },
       removeKeyword(keyword) {
         const words = this.keywords.filter(k => k !== keyword);