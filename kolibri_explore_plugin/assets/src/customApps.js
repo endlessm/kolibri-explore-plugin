40b02b32069b0ef9a73ff9b9c6c83bfefba46d2c
Manuel Quiñones <manuq@endlessos.org>
2023-10-20 13:21:22 -0300

    Remove custom presentation for discontinued channels
    - pbs-kids (38eaaf9ec82a44f9ab6e7a44cb730f07)
    - coronavirus (c8540424d77f44f8ae306e22d3b14eaf)
    - lgbtq (9eeebbc5140d5b98ac4a4d2ce9432135)
    - reading (59bb2e5a3d2e5e3b85b87d9ab4daa2f3)
    - code-org (6da97a60cbd44e918137827bd5425efb)
    - wikihow-arts-and-entertainment (58828121cc115549bda7fb3f71f4795e)
    - wikihow-cars-and-other-vehicles (05a4060cf5175df78edec8dc09e396a1)
    - wikihow-computers-and-electronics (69497db42937539e94fe3969f6995d2d)
    - wikihow-education-and-communications
    (00b0d8dff13e518d801ef816227b2691)
    - wikihow-food-and-entertaining (b9fe8fe9b92b5af5964feccdadb78413)
    - wikihow-hobbies-and-crafts (e114fe9de66e513384e4ca2e9b529c18)
    - wikihow-holidays-and-traditions (9c494b7b879e512f96f60176df85968c)
    - wikihow-home-and-garden (304d529d4b1c597ea7d3c59b30ac3c14)
    - wikihow-personal-care-and-style (afbf0498da3c5db3947c291ddda8e21a)
    - wikihow-sports-and-fitness (146efed5aa585229a49513219e73af66)
    - wikihow-work-world (e4e4d0e342f951b09aa9ef1622f5301f)
    - wikihow-youth (6f6dd2720bd2574fa70a069ac9b75046)

--- kolibri_explore_plugin/assets/src/customApps.js
+++ kolibri_explore_plugin/assets/src/customApps.js
@@ -8,7 +8,6 @@ export const CustomChannelApps = {
   c9d7f950ab6b5a1199e3d6c10d7f0103: 'khan-academy',
   '85b42a40745f4e2392ed62e72d4dad6e': 'oceanx',
   fc47aee82e0153e2a30197d3fdee1128: 'open-stax',
-  '38eaaf9ec82a44f9ab6e7a44cb730f07': 'pbs-kids',
   '197934f144305350b5820c7c4dd8e194': 'phet-interactive-simulations', // phet english
   '8fa678af1dd05329bf3218c549b84996': 'phet-interactive-simulations', // phet spanish
   e9d0d54d209344849e9bed0aa8c222ad: 'sikana-diy', // sikana english
@@ -18,7 +17,6 @@ export const CustomChannelApps = {
   '3160899a73564d8a8467284d9219b91c': 'terminal-two',
   '74f36493bb475b62935fa8705ed59fed': 'thoughtful-learning',
   bf0260ed911f44cda27a263db93a8512: '49ers-edu-digital-playbook',
-  c8540424d77f44f8ae306e22d3b14eaf: 'coronavirus',
   // Endless curated content (mostly Youtube content based)
   f061fce103ff5d4e9b8433e67802e666: 'arts',
   '4968191fba07548c9592fc174a70b5d6': 'beauty',
@@ -32,10 +30,8 @@ export const CustomChannelApps = {
   f5f6729f95b55753badeaa066fa6e986: 'healthy-body',
   '4e413158eac55422a5343af9fcfa8d59': 'healthy-mind',
   b40491d1ef8b5506b8c6ae861372e9de: 'jewelry-making',
-  '9eeebbc5140d5b98ac4a4d2ce9432135': 'lgbtq',
   '057f871caa405ec29d62ba0523c193d7': 'music',
   '914fee213ee146de869016c287116b23': 'novels',
-  '59bb2e5a3d2e5e3b85b87d9ab4daa2f3': 'reading',
   a8e6591f1afa426d859318a0a29d1237: 'samhsa',
   '3fcffebc58d15175b948b140434ef6e6': 'sports',
   '92e96efc082e5c62b0aac3847bdcdb33': 'staff-playlist',
@@ -51,7 +47,6 @@ export const CustomChannelApps = {
   '4b2f3f35debe4fbe96b77558a3364675': 'cas-es', // California Academy of Sciences (es)
   '08897e003ea9489eb3d86fc94ba08c21': 'support-ukraine',
   '608fd926be60462aba2feccba6c050c9': 'harper-collins',
-  '6da97a60cbd44e918137827bd5425efb': 'code-org',
   b6a8886278aa45a3a40c2e0ae5358115: 'pilas-engine',
   b06dd546e8ba4b44bf921862c9948ffe: 'wiixii',
   fed29d60e4d84a1e8dcfc781d920b40e: 'biblioteca-elejandria', // Biblioteca Elejandria
@@ -62,18 +57,6 @@ export const CustomChannelApps = {
   f446655247a95c0aa94ca9fa4d66783b: 'proyecto-biosfera', // Proyecto Biosfera
   // Zim file based channels
   f62db29be20453c4a267132e93a9e602: 'wikipedia',
-  '58828121cc115549bda7fb3f71f4795e': 'wikihow-arts-and-entertainment',
-  '05a4060cf5175df78edec8dc09e396a1': 'wikihow-cars-and-other-vehicles',
-  '69497db42937539e94fe3969f6995d2d': 'wikihow-computers-and-electronics',
-  '00b0d8dff13e518d801ef816227b2691': 'wikihow-education-and-communications',
-  b9fe8fe9b92b5af5964feccdadb78413: 'wikihow-food-and-entertaining',
-  e114fe9de66e513384e4ca2e9b529c18: 'wikihow-hobbies-and-crafts',
-  '9c494b7b879e512f96f60176df85968c': 'wikihow-holidays-and-traditions',
-  '304d529d4b1c597ea7d3c59b30ac3c14': 'wikihow-home-and-garden',
-  afbf0498da3c5db3947c291ddda8e21a: 'wikihow-personal-care-and-style',
-  '146efed5aa585229a49513219e73af66': 'wikihow-sports-and-fitness',
-  e4e4d0e342f951b09aa9ef1622f5301f: 'wikihow-work-world',
-  '6f6dd2720bd2574fa70a069ac9b75046': 'wikihow-youth',
 };
 
 export function getAppNameByID(id) {
